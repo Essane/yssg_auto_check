b544fb7b2dcfbd0f4efff85ff8e423088ea83919
Essane <961937111@qq.com>
2022-11-30 20:19:13 +0800

    修复只通知一次的bug

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# yssg_auto_check
-云上商工自动签到action
+# 云上商工自动签到action
+
+## 使用说明
+
+* 修改index.py内参数
+  * notifyToken 对应PushPlus的Token
+  * phone   手机号
+  * token   登录成功后返回的Token(在Cookies中取出)
--- index.py
+++ index.py
@@ -1,5 +1,5 @@
+# -- coding:UTF-8 --
 import time
-
 import execjs
 import requests
 from requests.adapters import HTTPAdapter
@@ -8,15 +8,10 @@ from retrying import retry
 from encrypt import EncryptClass
 
 user_data = [
-    # {
-    #     "notifyToken": "0ca2e6f891b44f17b91a0e1f1e73ba70",
-    #     "phone": "17772298110",
-    #     "token": "eyJhbGciOiJSUzUxMiJ9.eyJBVFRSX3VzZXJObyI6IjEzNTAwMDc0NjkiLCJzdWIiOiIxMzUwMDA3NDY5IiwiaXNzIjoiY2FzLnNxZ3h5LmVkdS5jbiIsImRldmljZUlkIjoiWU1RMjZaQUhoN0lEQUZUMWNMRld2UUJQIiwiQVRUUl9pZGVudGl0eVR5cGVJZCI6IjNlNmZjNDEwNDAzMTExZWNkZGZlODViMjNjOGFjMDlkIiwiQVRUUl9hY2NvdW50SWQiOiJlNzExMzc2MGZhMjIxMWVjNWM5NThiYzFiYmU1OGY4MyIsIkFUVFJfdXNlcklkIjoiZTZmY2VjMTBmYTIyMTFlYzVjOTU4YmMxYmJlNThmODMiLCJBVFRSX2lkZW50aXR5VHlwZUNvZGUiOiJUMDEiLCJBVFRSX2lkZW50aXR5VHlwZU5hbWUiOiLmlZnogYzlt6UiLCJBVFRSX29yZ2FuaXphdGlvbk5hbWUiOiLkv6Hmga_kuI7nlLXlrZDlt6XnqIvlrabpmaIiLCJBVFRSX3VzZXJOYW1lIjoi5p2o6ZSQIiwiZXhwIjoxNjcxNzE5Mzc2LCJBVFRSX29yZ2FuaXphdGlvbklkIjoiNDAxIiwiaWF0IjoxNjY5MTI3Mzc2LCJqdGkiOiJJZC1Ub2tlbi12em1vN0hEZ1JFTkVJdk1YIiwicmVxIjoiY29tLnN1cHdpc2RvbS5zcWd4eWFwcCIsIkFUVFJfb3JnYW5pemF0aW9uQ29kZSI6IjQwMSJ9.gYDYeRQPUxWlyW2vU93uT88THX1bMe1DgFylVP5K6MJzefmtwYCeQSI6IuNnqNIj5mzwfeYnClQr2b3ag1hG75FjUPlWwXMTQ_v5s5gbToAvCxCOVJywqNePT_6KPOlJsh8rZYcQNSVH6bqSXFIYXGMOsm6o3mZ4FOop5JcRt2WhRjQ4SxPxc6Wqm8I4cnUD3MWfWuV6tOv9enDKkcipHTjhWBeE6gKwwytNX4MDV-y0x-iLI8Fo_p7y6ttyukMnB7l-3xPMk7eiR2eHg3Gb7AZr88206nrsoFHxdiCsi74yiTkAa13gxOCHM4T4IgLosYLXXxzPcaxGUBxaHy6beg"
-    # },
     {
-        "notifyToken": "c16903a57912476e898db289784aeeee",
-        "phone": "13353610501",
-        "token": "eyJhbGciOiJSUzUxMiJ9.eyJBVFRSX3VzZXJObyI6IjMyMjAwMjAyMTUiLCJzdWIiOiIxMzUwMDA3NDc1IiwiaXNzIjoiY2FzLnNxZ3h5LmVkdS5jbiIsImRldmljZUlkIjoiMzE3REVCRDgtRDAyQi00NzJFLUE4OTAtOTM1RjI0NkIyNTIyIiwiQVRUUl9pZGVudGl0eVR5cGVJZCI6IjNlNmZjNDEwNDAzMTExZWNkZGZlODViMjNjOGFjMDlkIiwiQVRUUl9hY2NvdW50SWQiOiJmMjE5NzczMGZhMjIxMWVjNWM5NThiYzFiYmU1OGY4MyIsIkFUVFJfdXNlcklkIjoiMmE1NWI1MDA0ODdiMTFlY2I4OWE2ODE1NDc0ODk1YmQiLCJBVFRSX2lkZW50aXR5VHlwZUNvZGUiOiJUMDEiLCJBVFRSX2lkZW50aXR5VHlwZU5hbWUiOiLmlZnogYzlt6UiLCJBVFRSX29yZ2FuaXphdGlvbk5hbWUiOiLkv6Hmga_kuI7nlLXlrZDlt6XnqIvlrabpmaIiLCJBVFRSX3VzZXJOYW1lIjoi546L54Kz5r2tIiwiZXhwIjoxNjcxNTEyMDMwLCJBVFRSX29yZ2FuaXphdGlvbklkIjoiNDAxIiwiaWF0IjoxNjY4OTIwMDMwLCJqdGkiOiJJZC1Ub2tlbi11Wm95SjhvUzVDYlRUQ3h6IiwicmVxIjoiY29tLnN1cHdpc2RvbS5zcWd4eWFwcCIsIkFUVFJfb3JnYW5pemF0aW9uQ29kZSI6IjQwMSJ9.gOp9yQqD7angioCqOqlhlUvuTRgB302bRtSeb9Y_oRLPplVo3UedxMzvbnv6TvKFdHigz1WPLTf2zSIVx6WVhnjxXrtNlh3K9cy18grVVAIRpY1HpBT5hbjuLlfk7PZXlsgOkOR-etl2xlWHxgJ52hCxjWZStVBvlaqPaZENM25UP-CTn1QI0DkIrg3yReMd3-aWlmOYvTGUOAmUiAdprycHs2mqKpSaWQE3Zn8GDAL5mJVQWIowjTC6zMPKGfJdiNqkzu9hbaad7amjXj7u1HOxxIkLkno67i-GdVAT6NNEYuMI95ZIJcQLox5faxii81zqz43NZZ9-ZUFAqcLhUA"
+        "notifyToken": "",
+        "phone": "",
+        "token": ""
     }
 ]
 
@@ -44,7 +39,8 @@ def generate_data_header(token, phone):
         "User-Agent": "Mozilla/5.0 (Linux; Android 8.1.0; Pixel Build/OPM4.171019.021.P1; wv) AppleWebKit/537.36 ("
                       "KHTML, like Gecko) Version/4.0 Chrome/80.0.3987.87 Mobile Safari/537.36 SuperApp"
     }
-    resp = session.get(url="https://formflow.sqgxy.edu.cn/formflow/v1/user/getUserInfo2", headers=headers, timeout=5)
+    resp = session.get(
+        url="https://formflow.sqgxy.edu.cn/formflow/v1/user/getUserInfo2", headers=headers, timeout=5)
     if not resp.json()['flag']:
         return False, resp.json()['msg']
     headers.update({
@@ -83,7 +79,7 @@ def commit_act_form(token, phone):
                 ));
                 return e
             };
-        """)  # 获取代码编译完成后的对象
+        """)
         headers.update({
             "visitor": "0",
             "timeStr": t,
@@ -97,7 +93,8 @@ def commit_act_form(token, phone):
 
 
 def is_retry(exception):
-    notifyList.append(str(exception))
+    notifyList.append("%s: 第%d次打卡：%s" % (
+        str(time.strftime("%Y-%m-%d %H:%M:%S")), len(notifyList)+1, str(exception)))
     return True
 
 
@@ -105,30 +102,33 @@ def is_retry(exception):
 def run(item):
     result = False
     msg = None
-    for count in range(5):
-        count += 1
-        result, msg = commit_act_form(item['token'], item['phone'])
-        notify(item['notifyToken'], "打卡通知",
-               "%s: 第%d打卡：%s" % (str(time.strftime("%Y-%m-%d %H:%M:%S")), count, msg))
-        if result:
-            break
+    result, msg = commit_act_form(item['token'], item['phone'])
     if not result:
-        msg = "打卡失败,已重试5次！"
+        raise RuntimeError(msg)
+    return result, msg
 
 
 def main():
+    result = False
+    msg = None
+    msgList = []
     item = None
-    try:
-        for item in user_data:
-            run(item)
-    except Exception as e:
-        print(e.args)
-        notify(item['notifyToken'], "打卡通知", '\n'.join(list(map(str, notifyList))))
+    for item in user_data:
+        try:
+            result, msg = run(item)
+            print(result, msg)
+        except Exception as e:
+            print(e.args)
+        finally:
+            if not result:
+                msg = '\n'.join(list(map(str, notifyList)))
+            notify(item['notifyToken'], "打卡通知", msg)
+            msgList.append(msg)
+    return msgList
 
 
 def main_handler(event, context):
     return main()
 
 # if __name__ == '__main__':
-#     notify("c16903a57912476e898db289784aeeee", "测试", "内容")
 #     main()
